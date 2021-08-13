d4fc5aa7f8e8e2b15aab766b23f1202c707ee122
yiting <manana034@hotmail.com>
2021-08-13 09:26:17 +0800

    我加了數學版本喔

--- gitbranchsample/src/gitbranchsample/MainApp.java
+++ gitbranchsample/src/gitbranchsample/MainApp.java
@@ -2,7 +2,16 @@ package gitbranchsample;
 
 public class MainApp {
 	public static void main(String[] args) {
-		System.out.print("1st Version :)");		
-		System.out.print("2nd Version :)");		
+		System.out.print("1st Version :)");
+		System.out.print("2nd Version :)");
+		// 這是一個新加的數學運算版本
+		Math cMath = new Math();
+		System.out.print(cMath.add(10, 5));
+	}
+}
+
+class Math {
+	int add(int a, int b) {
+		return a + b;
 	}
 }
